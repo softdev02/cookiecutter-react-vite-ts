c037b248b0e614c86b28a1f6403590dff144ebaa
Saranraj S <140391665+softdev02@users.noreply.github.com>
2025-02-01 00:28:47 +0530

    Add files via upload

---
+++ {{cookiecutter.project_name}}/js-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello {{ cookiecutter.project_name }}</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
